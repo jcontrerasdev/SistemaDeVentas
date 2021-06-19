8f8cc083d1d55212073281036fb402a42e081f29
Juan Carlos Contreras <cmjcarlos7@gmail.com>
2021-06-20 00:03:30 +0200

    Servicio y solicitud Http para cliente.

---
+++ Frontend/app/src/app/services/apicliente.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiclienteService {
+
+  url: string = 'https://localhost:44349/api/cliente';
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  getClientes(): Observable<Response>{
+    return this._http.get<Response>(this.url);
+  }
+}
+

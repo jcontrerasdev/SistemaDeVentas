66397058c1a2c84284c9ed9f82931ab76a4b6dc4
Juan Carlos Contreras <cmjcarlos7@gmail.com>
2021-08-21 12:33:45 +0200

    Validaciones de modelos con DataNotation

--- Backend/WSVentas/Models/Request/VentaRequest.cs
+++ Backend/WSVentas/Models/Request/VentaRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,12 @@ namespace WSVentas.Models.Request
 {
     public class VentaRequest
     {
+        [Required]
+        [Range(1, Double.MaxValue, ErrorMessage ="El valor del idCliente debe ser mayor de 0")]
+        [ExisteCliente(ErrorMessage ="El cliente no existe")]
         public int IdCliente { get; set; }
-        public decimal Total { get; set; }
+        [Required]
+        [MinLength(1, ErrorMessage ="Deben existir conceptos")]
         public List<Concepto> Conceptos { get; set; }
         public VentaRequest()
         {
@@ -23,4 +28,19 @@ namespace WSVentas.Models.Request
         public decimal Importe { get; set; }
         public int IdProducto { get; set; }
     }
+
+    #region Validations
+    public class ExisteClienteAttribute: ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int idCliente = (int)value;
+            using (var db = new Models.VentaRealContext())
+            {
+                if (db.Clientes.Find(idCliente) == null) return false;
+            }
+            return true;
+        }
+    }
+    #endregion
 }
